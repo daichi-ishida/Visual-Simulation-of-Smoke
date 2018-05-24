7d37444867cf8d9cbf1ee8bf693983ec3d770526
Daichi Ishida <daichi.phys.lab@gmail.com>
2018-05-24 15:23:40 +0900

    add GridView Mode

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -4,7 +4,7 @@
 #include "Camera.hpp"
 #include "constants.hpp"
 
-Camera::Camera() : m_position(glm::vec3(0.0f, 0.0f, 5.0f)),
+Camera::Camera() : m_position(glm::vec3(0.0f, 0.0f, 4.0f * (float)Nz / 3.0f)),
                    m_horizontalAngle(M_PI),
                    m_verticalAngle(0),
                    m_FoV(45.0f),
@@ -21,26 +21,17 @@ Camera::~Camera()
 void Camera::update()
 {
     // FPScontroll();
+    GridViewControll();
 }
 
 void Camera::GridViewControll()
 {
-    glm::vec3 center = glm::vec3((float)Nx * MAGNIFICATION / 2.0f, (float)Ny / 2.0f, (float)Nz / 2.0f);
+    glm::vec3 center = glm::vec3((float)Nx * MAGNIFICATION / 2.0f, (float)Ny * MAGNIFICATION / 2.0f, (float)Nz * MAGNIFICATION / 2.0f);
     m_currentTime = glfwGetTime();
     float deltaTime = m_currentTime - m_lastTime;
 
     GLFWwindow *window = glfwGetCurrentContext();
 
-    // Get mouse position
-    double xpos, ypos;
-    glfwGetCursorPos(window, &xpos, &ypos);
-
-    // Reset mouse position for next frame
-    glfwSetCursorPos(window, WIN_WIDTH / 2, WIN_HEIGHT / 2); //reset mouse cursor position to the center wof window
-
-    // Compute new orientation
-    m_horizontalAngle += m_mouseSpeed * float(WIN_WIDTH / 2 - xpos);
-    m_verticalAngle += m_mouseSpeed * float(WIN_HEIGHT / 2 - ypos);
     //restrict vertical angle not to be upside down
     if (m_verticalAngle < -M_PI / 2)
     {
@@ -52,19 +43,17 @@ void Camera::GridViewControll()
     }
 
     // Direction : Spherical coordinates to Cartesian coordinates conversion
-    glm::vec3 direction(
-        std::cos(m_verticalAngle) * std::sin(m_horizontalAngle),
-        std::sin(m_verticalAngle),
-        std::cos(m_verticalAngle) * std::cos(m_horizontalAngle));
+    glm::vec3 direction(center - m_position);
+    glm::normalize(direction);
 
     // Right vector
     glm::vec3 right = glm::vec3(
-        std::sin(m_horizontalAngle - M_PI / 2.0f), //substitute m_verticalAngle = 0 in the direction vector
+        -std::sin(m_horizontalAngle), //substitute m_verticalAngle = 0 in the direction vector
         0,
-        std::cos(m_horizontalAngle - M_PI / 2.0f));
+        std::cos(m_horizontalAngle));
 
     // Up vector
-    glm::vec3 up = glm::cross(right, direction); //up vector which is perpendicular to right and direction , by definition
+    glm::vec3 up = glm::cross(-right, direction); //up vector which is perpendicular to right and direction , by definition
 
     // Move forward
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
@@ -98,13 +87,13 @@ void Camera::GridViewControll()
     }
 
     // Projection matrix : 45ｰ Field of View,  ratio, display range : 0.1 unit <-> 100 units
-    m_projectionMatix = glm::perspective(glm::radians(m_FoV), (float)WIN_WIDTH / (float)WIN_HEIGHT, 0.1f, 100.0f);
+    m_projectionMatix = glm::perspective(glm::radians(m_FoV), (float)WIN_WIDTH / (float)WIN_HEIGHT, 0.1f, 1000.0f);
 
     // Camera matrix
     m_viewMatrix = glm::lookAt(
-        m_position,             // Camera is here
-        m_position + direction, // and looks here : at the same position, plus "direction"
-        up                      // Head is up (set to 0,-1,0 to look upside-down)
+        m_position, // Camera is here
+        center,     // and looks here : at the same position, plus "direction"
+        up          // Head is up (set to 0,-1,0 to look upside-down)
     );
 
     // For the next frame, the "last time" will be "now"
@@ -151,7 +140,7 @@ void Camera::FPScontroll()
         std::cos(m_horizontalAngle - M_PI / 2.0f));
 
     // Up vector
-    glm::vec3 up = glm::cross(right, direction); //up vector which is perpendicular to right and direction , by definition
+    glm::vec3 up = glm::cross(-right, direction); //up vector which is perpendicular to right and direction , by definition
 
     // Move forward
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
@@ -185,7 +174,7 @@ void Camera::FPScontroll()
     }
 
     // Projection matrix : 45ｰ Field of View,  ratio, display range : 0.1 unit <-> 100 units
-    m_projectionMatix = glm::perspective(glm::radians(m_FoV), (float)WIN_WIDTH / (float)WIN_HEIGHT, 0.1f, 100.0f);
+    m_projectionMatix = glm::perspective(glm::radians(m_FoV), (float)WIN_WIDTH / (float)WIN_HEIGHT, 0.1f, 1000.0f);
 
     // Camera matrix
     m_viewMatrix = glm::lookAt(
